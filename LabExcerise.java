ac5b7b22a5a502c8a47d2e8e9f5a0bc8cc93b3bf
RobinRagas123 <raphaelragas3@gmail.com>
2024-11-13 09:15:51 +0800

    Add files via upload

---
+++ LabExcerise.java
@@ -0,0 +1,20 @@
+
+package labexcerise;
+
+public class LabExcerise {
+
+    
+    public static void main(String[] args) {
+      int iNumber = 3;
+      
+      if (iNumber >= 0){
+          if (iNumber == 0){
+              System.out.println("Frist String");
+          }
+          else{
+              System.out.println("Second String");
+          }
+      }
+      System.out.println("Third String");
+    }  
+}   
